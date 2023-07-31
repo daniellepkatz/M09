5a934c7b03cd87537a0bb361414f8af1a62e690e
daniellepkatz <ujj2wd@virginia.edu>
2023-07-31 11:25:41 -0400

    push

---
+++ mymodule.py
@@ -0,0 +1,8 @@
+def say_hi(x):
+    '''
+    Says hi to user when ready
+
+    '''
+    if x == 'ready':
+        print('Hi user!')
+
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='mymodule',
+    version='1.0.0',
+    url='https://github.com/daniellepkatz/MSDS-M09-Exercise',
+    author='Danielle Katz',
+    author_email='ujj2wd@viriginia.edu',
+    description='My package says hi to the user',
+    packages=find_packages(), 
+    license='MIT',   
+    install_requires=[],
+)
